25fe33a1e2d6edb1666505aabb1278b772036ab9
unknown <86hirose@gmail.com>
2020-05-06 18:34:24 +0900

    いろいろ追加など

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-
 client.on('ready', () => {
   console.log(`${client.user.username} でログインしています。`)
 })
 client.on('message', async msg => {
+    const fs = require('fs');
     var kengen = msg.member.roles.has('707408548019306556');
     var str =  msg.content;
     const rsp = /^[rsp]$/g;
+    const file = msg.attachments.first();
+    var data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+    var jsonObject = JSON.parse(fs.readFileSync('./joke.json', 'utf8'));
   if(str === '!npee') {
     msg.channel.send('ﾝﾋﾟｰｰｰｰｰｰwwww');
   }else if(msg.content === '!oreka'){
@@ -40,18 +43,10 @@ client.on('message', async msg => {
     if (!kengen){
       msg.channel.send('⚠コマンドの実行に失敗しました。権限がありません。');
     }else{
-      if(str.substring(7) == "jsontest"){
-        const fs = require('fs');
-        const jsonObject = JSON.parse(fs.readFileSync('./joke.json', 'utf8'));
-        msg.channel.send(jsonObject[0]);
-      }else if(str.substring(7,15) == "jsonadd "){
-        const fs = require('fs');
-        var jsonObject = JSON.parse(fs.readFileSync('./joke.json', 'utf8'));
+      if(str.substring(7,15) === "jsonadd "){
         jsonObject.push(str.substring(15));
         fs.writeFileSync('./joke.json', JSON.stringify(jsonObject));
       }else if(str.substring(7) === "joke"){
-        const fs = require('fs');
-        const jsonObject = JSON.parse(fs.readFileSync('./joke.json', 'utf8'));
         msg.channel.send(jsonObject[Math.floor(Math.random()*jsonObject.length)]);
       }
       msg.channel.send('✅コマンドの実行に成功しました。');
@@ -61,7 +56,7 @@ client.on('message', async msg => {
     msg.delete();
     msg.channel.send(txt);
   }else if(str == "!help"){
-    const m = "---<command list>---\n!dice              6面ダイスを振ります\n!npee             ﾝﾋﾟｰｰｰｰｰｰwwww\n!tokumei *     botが代わりに発言してくれます\n!marry *        *に求婚します\n!rsp [r|s|p] じゃんけんです\n!debug           デバッグ用です\n!help              コマンドリストを表示します\n!joke              るるたちゃんの鉄板ジョークを聞きたいか？"
+    const m = "---<command list>---\n!dice              6面ダイスを振ります\n!npee             ﾝﾋﾟｰｰｰｰｰｰwwww\n!tokumei *     botが代わりに発言してくれます\n!marry *        *に求婚します\n!rsp [r|s|p] じゃんけんです\n!debug           デバッグ用です\n!help              コマンドリストを表示します\n!joke              るるたちゃんの鉄板ジョークを聞きたいか？\n!addjoke           るるたちゃんの鉄板ジョークを追加！(要権限)"
     msg.channel.send(m);
   }else if(str == "!joke"){
     const fs = require('fs');
@@ -79,16 +74,38 @@ client.on('message', async msg => {
     }
   }else if(str == "!激ヤバ腹筋崩壊最強面白ギャグ"){
     msg.channel.send("undefind");
-  }else if(str == "さて"){
+  }else if(str == "さて"&&flag){
     msg.channel.send("さてじゃないんだよ");
-    msg.member.send('Try again:https://discord.gg/ZF6vAdN')
+    //msg.member.send('Try again:https://discord.gg/ZF6vAdN')
     msg.member.kick();
-  }else if(str == ':emoji_38:'){
+  }else if(str == '<:emoji_38:705716399104065556>'&&flag){
     msg.channel.send("殺してやるよ");
-    msg.member.send('Try again:https://discord.gg/ZF6vAdN')
+    msg.channel.send(msg.member.user.tag+"は無残な姿で発見されました。");
     msg.member.kick();
-    msg.channel.send(`_${member.user.tag}は無残な姿で発見されました。_`)
+    //msg.member.send('Try again:https://discord.gg/ZF6vAdN')
+    //なぜか動かない　メッセージ送信が許可されてないだけだと思う
+  }else if(str == "!kill_off"){
+    if(!kengen){
+      msg.channel.send('⚠コマンドの実行に失敗しました。権限がありません。');
+    }else{
+      data.kill = false;
+      fs.writeFileSync('./data.json', JSON.stringify(data));
+      msg.channel.send('✅コマンドの実行に成功しました。');
+    }
+  }else if(str == "!kill_on"){
+    if(!kengen){
+      msg.channel.send('⚠コマンドの実行に失敗しました。権限がありません。');
+    }else{
+      data.kill = true;
+      fs.writeFileSync('./data.json', JSON.stringify(data));
+      msg.channel.send('✅コマンドの実行に成功しました。');
+    }
+  }else if(str == "!icon"){
+    if (!file) return // 添付ファイルがなかったらスルー
+    if (!file.height && !file.width) return // 画像じゃなかったらスルー
+    client.user.setAvatar(file.url)
   }
+  console.log(str);
 });
 
 client.login('NzA3Mjg5MzIwMzA1NzIxMzU0.XrJCww.ICXpIwz2rMfOqBIixMtM7X0Ik3E');
