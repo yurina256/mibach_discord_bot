0b25eaec0261e6ee8be22ad80b63278e7f45b85c
yurina256 <info@yurina256.net>
2021-12-01 22:14:37 +0900

    small fix 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Intents, Client } = require("discord.js");
 const options = {
   intents: ["GUILDS", "GUILD_MESSAGES","GUILD_MEMBERS"],
 };
-const client = new Client()
+const client = new Client(options)
 var fukubiki = [];
 class Random {
   constructor(seed = 88675123) {
